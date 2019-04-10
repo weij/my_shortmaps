69c7c047d1f5984b1662ce4bc70a1b2dfc4e017f
Weijun Yin <weijun.yin@letote.cn>
2019-04-10 08:33:43 +0800

    add steps notes

--- lib/my_shortmaps.ex
+++ lib/my_shortmaps.ex
@@ -4,11 +4,24 @@ defmodule MyShortmaps do
   """
   @default_modifier ?s
 
+  #
+  # 步骤1: 先上个bodyless function
+  #
   defmacro sigil_m(term, modifiers)
+
+  #
+  # 步骤2: quote expression 固定写法, 为什么? 自己看看sigil_m的quote expresion便明白了
+  #
+  # iex(1)> quote do: ~m("hello" :weijun)
+  # iex(2)> {:sigil_m, [], [{:<<>>, [], ["\"hello\" :weijun"]}, []]}
+  #
   defmacro sigil_m({:<<>>, line, [string]}, modifiers) do
     sigil_m_function(line, String.split(string), modifier(modifiers), __CALLER__)
   end
 
+  #
+  # 步骤3:
+  #
   def sigil_m_function(line, words, modifier, _caller) do
     pairs = make_pairs(words, modifier)
     IO.inspect(line, label: "line")
@@ -18,8 +31,9 @@ defmodule MyShortmaps do
 
   def make_pairs(words, modifier) do
     keys = Enum.map(words, &strip_pin/1)
+    IO.inspect(keys, label: "after stripped pin")
     variables = Enum.map(words, &handle_var/1)
-
+    IO.inspect(variables, label: "get value input")
     # ensure_valid_variable_names(keys)
 
     case modifier do
